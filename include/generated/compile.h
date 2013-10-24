9df8cc1d04ca3da2199d7da4d0554ec73aa75f95
root <root@rocky.(none)>
2013-10-25 06:32:38 +0800

    it's origin

---
+++ include/generated/compile.h
@@ -0,0 +1,6 @@
+/* This file is auto generated, version 439 */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#439 Sun Sep 23 13:36:30 CST 2012"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "forlinx"
+#define LINUX_COMPILER "gcc version 4.3.2 (Sourcery G++ Lite 2008q3-72) "
